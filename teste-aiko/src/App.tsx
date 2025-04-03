ac3da78f991ae1952b4e6306874d43e475bbcc6c
Bianca <you@example.com>
2025-04-03 17:52:48 -0300

    adds initial layout

--- teste-aiko/src/App.tsx
+++ teste-aiko/src/App.tsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-slate-300 grid grid-rows-[auto_1fr] h-screen">
+      <nav className="text-blue-950 p-4 font-extralight flex items-center">
+        <img src="/img/aiko.png" alt="Logo" className="h-10 w-auto mx-2" />
+        <span>| Gestão de Equipamentos</span>
+      </nav>
+    </div>
   )
 }
 
